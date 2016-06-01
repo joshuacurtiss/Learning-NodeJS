0d95c9e4c6dae92f84d8b4a94ec5ddd322edd013
Josh Curtiss <git@curtiss.me>
2016-06-01 08:24:12 -0500

    Created a web server that serves some static files.
    But we still have control over what kind of stuff is requested.

---
+++ 3 fileserver/fileserver.js
@@ -0,0 +1,35 @@
+var port=3000;
+var http = require("http");
+var fs = require('fs');
+var path = require("path");
+
+http.createServer(function(req,res){
+    console.log(`${req.method} ${req.url}`)
+    if(req.url==="/")
+    {
+        fs.readFile("./public/index.htm","UTF-8",function(err,html){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.end(html);
+        });
+    }
+    else if( req.url.match(/.css$/) )
+    {
+        var cssPath = path.join(__dirname,"public",req.url);
+        var fileStream = fs.createReadStream(cssPath,"UTF-8");
+        res.writeHead(200,{"Content-Type":"text/css"});
+        fileStream.pipe(res);
+    }
+    else if( req.url.match(/.jpg$/) )
+    {
+        var imgPath=path.join(__dirname,"public",req.url);
+        var imgStream = fs.createReadStream(imgPath);
+        res.writeHead(200,{"Content_Type":"image/jpeg"});
+        imgStream.pipe(res);
+    }
+    else
+    {
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end("404 File Not Found!");
+    }
+}).listen(port);
+console.log("Running on port "+port+".");
