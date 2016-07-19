fd45ed18149381b6b73c08e2ab63cb0e3e6db389
Josh Curtiss <josh@curtiss.me>
2016-07-19 17:38:37 -0500

    Implemented a legacy table entity. Used the classic Employee Directory "empdir" table. Created an "Emp" entity that represents that table with the legacy name and legacy fields (not all of them). Then, run a sampleUsage() function that just outputs the names of all the Johns.

--- 4 sequelize/app.js
+++ 4 sequelize/app.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+// Sample Function
+function sampleUsage()
+{
+    Emp.findAll({ where: {fname:'John'}, order: 'lname' }).then(function(emps) {
+        emps.forEach(function(e){
+            console.log(`${e.get('fname')} ${e.get('lname')}`);
+        });
+    });
+}
+
 // Dependencies
 var env = process.env.NODE_ENV || 'development';
 var config=require(__dirname + "/config.json")[env];
@@ -8,6 +18,24 @@ var sequelize=new Sequelize(config.name,config.user,config.password,{
     host:config.host,
     dialect:config.dialect,
     pool: {max:5,min:0,idle:10000}
+});
+
+// Init Model
+var Emp = sequelize.define('emp', {
+    id:             {type: Sequelize.STRING,    field: 'eID',      primaryKey: true},
+    fname:          {type: Sequelize.STRING,    field: 'eFName'},
+    lname:          {type: Sequelize.STRING,    field: 'eLName'},
+    mname:          {type: Sequelize.STRING,    field: 'eMName'},
+    nickname:       {type: Sequelize.STRING,    field: 'eNickname'},
+    positiontitle:  {type: Sequelize.STRING,    field: 'ePosTitle'},
+    enddate:      {type: Sequelize.DATE,        field: 'eEndDate'}
+},{
+    timestamps:false,
+    tableName:'empdir'
+});
+Emp.sync().then(function () {
+    console.log("Emp sync done.");
+    sampleUsage();
 });
 
 sequelize
