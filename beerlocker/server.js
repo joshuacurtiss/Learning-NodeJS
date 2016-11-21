7c3f656576c4600daacd8370a2a473a032a1b9cb
Josh Curtiss <josh@curtiss.me>
2016-11-21 17:18:14 -0600

    Setting up the OAUTH2 server.

--- beerlocker/server.js
+++ beerlocker/server.js
@@ -1,14 +1,26 @@
 // Set up Express
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 var app = express();
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
 
 // Use the body-parser package in our application
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// Set up Password
+// Use express session support since OAuth2orize requires it
+app.use(session({
+  secret: 'Super Secret Session Key',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Set up Passport
 var passport = require('passport');
 app.use(passport.initialize());
 
@@ -16,6 +28,8 @@ app.use(passport.initialize());
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
+var clientController = require('./controllers/client');
+var oauth2Controller = require('./controllers/oauth2');
 
 // Connect to the beerlocker MongoDB
 var mongoose = require('mongoose');
@@ -27,6 +41,11 @@ var port = process.env.PORT || 3000;
 // Create our Express router
 var router = express.Router();
 
+// Create endpoint handlers for /clients
+router.route('/clients')
+  .post(authController.isAuthenticated, clientController.postClients)
+  .get(authController.isAuthenticated, clientController.getClients);
+  
 // Create endpoint handlers for /beers
 router.route('/beers')
   .post(authController.isAuthenticated, beerController.postBeers)
@@ -42,6 +61,15 @@ router.route('/beers/:beer_id')
 router.route('/users')
   .post(userController.postUsers)
   .get(authController.isAuthenticated, userController.getUsers);
+
+// Create endpoint handlers for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+// Create endpoint handlers for oauth2 token
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 // Register all our routes with /api
 app.use('/api', router);
