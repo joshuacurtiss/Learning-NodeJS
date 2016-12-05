2a530804f007b22798ca43647e6d6e62625f3317
Josh Curtiss <josh@curtiss.me>
2016-12-05 15:06:46 -0600

    Updated user object to have more properties.

--- expressjwt/model/user.js
+++ expressjwt/model/user.js
@@ -1,23 +1,57 @@
 class User {
     
     constructor( params={} ) {
-        this.id=params.id || "";
-        this.fname=params.fname || "";
-        this.lname=params.lname || "";
-        this.positionshorttitle=params.positionshorttitle || "";
-        this.email=params.email || "";
-        this.usertype=params.usertype || "";
+        const stringprops=[
+            "id",
+            "usertype",
+            "fname",
+            "mname",
+            "lname",
+            "siteid",
+            "sitename",
+            "bldgid",
+            "bldgname",
+            "room",
+            "email",
+            "fullphone",
+            "cell",
+            "positionid",
+            "positiontitle",
+            "positionshorttitle",
+            "grade",
+            "schoolid",
+            "schoolname",
+            "divid",
+            "divname",
+            "deptid",
+            "deptname",
+            "sex",
+            "bargunitid",
+            "bargunitname",
+            "rankid",
+            "rankname",
+            "classid",
+            "classname",
+            "exemptid",
+            "exemptname"
+        ];
+        const dateprops=[
+            "startdate",
+            "enddate"
+        ];
+        // Set all string and date properties:
+        for( var prop of stringprops ) {this[prop]=params[prop] || ""}
+        for( var prop of dateprops ) {this[prop]=new Date(params[prop])}
+        // Set the supervisor if one exists:
+        this.sup=(typeof params.sup=="object")?new User(params.sup):null; 
     }
 
     get found() {return (this.id!="")}
     get name() {return (this.fname+" "+this.lname).trim()}
-    get title() {return this.positionshorttitle}
+    get title() {return this.positiontitle}
     get fullid() {return this.usertype.toLowerCase()+this.id}
 
-    toString() {
-        var out=`${this.fname} ${this.lname} ` + (this.fullid.length?`(${this.fullid}) `:"") + (this.title.length?`(${this.title}) `:"") + (this.email.length?`<${this.email}>`:"");
-        return out.trim();
-    }
+    toString() {return (`${this.fname} ${this.lname} ` + (this.fullid.length?`(${this.fullid}) `:"") + (this.title.length?this.title:"")).trim()}
 }
 
 module.exports=User;
