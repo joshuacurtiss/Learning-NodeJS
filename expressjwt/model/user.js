5a9b4e6bae388993af12ca4568d4432a241b7d06
Josh Curtiss <josh@curtiss.me>
2016-12-05 11:51:29 -0600

    Set up a sample API that is very simple. No authentication. It just lists and looks up users.  The user data is fake, pulling from the config file. Just for demo purposes.

---
+++ expressjwt/model/user.js
@@ -0,0 +1,21 @@
+class User {
+    
+    constructor( params={} ) {
+        this.id=params.id || "";
+        this.fname=params.fname || "";
+        this.lname=params.lname || "";
+        this.title=params.title || "";
+        this.email=params.email || "";
+    }
+
+    get found() {
+        return (this.id!="");
+    }
+
+    toString() {
+        var out=`${this.fname} ${this.lname} ` + (this.id.length?`(${this.id}) `:"") + (this.title.length?`(${this.title}) `:"") + (this.email.length?`<${this.email}>`:"");
+        return out.trim();
+    }
+}
+
+module.exports=User;
--- expressjwt/server.js
+++ expressjwt/server.js
@@ -1,19 +1,50 @@
-var config = require("config");
-var express = require('express')
-var app = express()
+var config      = require("config");
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var jwt         = require('jsonwebtoken');
+var util        = require('util');
+var User        = require("./model/user.js");
 
 const port=config.get("port");
 
-app.use(function(req,res,next){
-  var d=new Date().toLocaleString();
-  console.log(d + ": " + req.method + " " + req.url);
-  next();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res)=>{
+  var apiurl=`http://${req.headers.host}/api`;
+  res.send(`Hello! Access the API at <a href="${apiurl}">${apiurl}</a>.`)
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello!')
-})
+var apiRoutes=express.Router();
 
-app.listen(port, function () {
+apiRoutes.get("/", (req, res)=>{
+  res.json({message:"Welcome to my awesome API."});
+});
+
+apiRoutes.get("/users", (req, res)=>{
+  var users=config.get("users");
+  res.json(users);
+});
+
+apiRoutes.get("/users/:id", (req, res)=>{
+  var users=config.get("users");
+  var user=new User();
+  var i=0;
+  while( i<users.length ) {
+    var u=new User(users[i++]);
+    if( u.id.toLowerCase()==req.params.id.toLowerCase() ) {
+      user=u;
+      i=users.length;
+    }
+  }
+  res.json(user);
+});
+
+app.use("/api", apiRoutes);
+
+app.listen(port, ()=>{
   console.log('Listening on port '+port+'!')
-})
+});
