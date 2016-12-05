9969bd2c5b9564563d7c954a13b0f876ec25a906
Josh Curtiss <josh@curtiss.me>
2016-12-05 14:15:43 -0600

    Just some more improvements to User class. Will accept positionshorttitle but has an alias for just title. Has a fullid property that combined usertype and id. Added a name property that combined fname/lname.

--- expressjwt/model/user.js
+++ expressjwt/model/user.js
@@ -4,21 +4,18 @@ class User {
         this.id=params.id || "";
         this.fname=params.fname || "";
         this.lname=params.lname || "";
-        this.title=params.title || "";
+        this.positionshorttitle=params.positionshorttitle || "";
         this.email=params.email || "";
+        this.usertype=params.usertype || "";
     }
 
-    get found() {
-        return (this.id!="");
-    }
-
-    get name() {
-        var out=this.fname+" "+this.lname;
-        return out.trim();
-    }
+    get found() {return (this.id!="")}
+    get name() {return (this.fname+" "+this.lname).trim()}
+    get title() {return this.positionshorttitle}
+    get fullid() {return this.usertype.toLowerCase()+this.id}
 
     toString() {
-        var out=`${this.fname} ${this.lname} ` + (this.id.length?`(${this.id}) `:"") + (this.title.length?`(${this.title}) `:"") + (this.email.length?`<${this.email}>`:"");
+        var out=`${this.fname} ${this.lname} ` + (this.fullid.length?`(${this.fullid}) `:"") + (this.title.length?`(${this.title}) `:"") + (this.email.length?`<${this.email}>`:"");
         return out.trim();
     }
 }
