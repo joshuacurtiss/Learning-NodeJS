a73ebf96de2468905d01b83668d3484e18b2ac51
Josh Curtiss <git@curtiss.me>
2016-12-23 00:08:53 -0600

    Added button panel for settings and quit.
    Currently just quit works. Settings feature is coming.

--- videoplayerwithcountdown/main.js
+++ videoplayerwithcountdown/main.js
@@ -2,8 +2,11 @@ const electron = require("electron");
 const {app, BrowserWindow} = electron;
 
 app.on("ready", () => {
-    let win=new BrowserWindow({width:640, height:480});
-    win.loadURL(`file://${__dirname}/index.html`);
-    win.setFullScreen(true);          // Example of setting app to run full-screen
-    //win.webContents.openDevTools();   // Opening the dev tools
+    var videoWin=new BrowserWindow({width:640, height:480, fullscreen:true, frame:false});
+    videoWin.loadURL(`file://${__dirname}/index.html`);
+    //videoWin.webContents.openDevTools();   // Opening the dev tools
 });
+
+exports.quit=()=>{
+    app.quit();
+}
